576b920cbde85da22a7adedffa7baebadeeae9f1
einkelflugle <cluckyferret@gmail.com>
2014-12-16 14:07:34 +1000

    Added name field to users

--- db/schema.rb
+++ db/schema.rb
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20141212033954) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
